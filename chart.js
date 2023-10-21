21595608061d069c5b46dcbd3058d641821dbf64
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-10-21 14:36:06 -0300

    Consertando bugs de repositório e arquivo

--- chart.js
+++ chart.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 let myDoughnutChart; // Variable to store the chart instance
@@ -36,42 +35,3 @@ export function createDoughnutChart(chartId, receipts, expenses) {
     },
   });
 }
-=======
-"use strict";
-
-let myDoughnutChart; // Variable to store the chart instance
-
-// Chart module
-export function createDoughnutChart(chartId, receipts, expenses) {
-  if (myDoughnutChart) {
-    // If a previous chart instance exists, destroy it
-    myDoughnutChart.destroy();
-  }
-
-  receipts === 0 && expenses === 0 ? ((receipts = 1), (expenses = 1)) : null;
-
-  const ctx = document.getElementById(chartId).getContext("2d");
-  myDoughnutChart = new Chart(ctx, {
-    type: "doughnut",
-    data: {
-      labels: ["Receipts", "Expenses"],
-      datasets: [
-        {
-          label: "Doughnut Chart",
-          data: [receipts, expenses],
-          backgroundColor: ["#40c057", "#fa5252"],
-        },
-      ],
-    },
-    options: {
-      plugins: {
-        legend: {
-          display: true,
-          position: "bottom",
-        },
-      },
-      // Optionally, you can customize other chart options here.
-    },
-  });
-}
->>>>>>> a0bc7cb35f99dbeb96e4e533e7e23cbed8c68b88
